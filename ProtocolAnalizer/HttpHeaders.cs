ddb5c4b21d0a98270808c3b5c0dca42c792d3a31
Aliaksandr Orgish <aliaksandr_orgish@epam.com>
2015-05-13 16:51:05 +0300

    Add init version of HttpHeaders.cs

---
+++ ProtocolAnalizer/HttpHeaders.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolAnalizer {
+
+    public enum HttpServerName { 
+        Other = 0,
+        Apache = 1,
+        IIS = 2,
+        ngnix = 3,
+        GSE = 4,
+        LiteSpeed = 5,
+        lighttpd = 6,
+        uServ = 7,
+        ATS = 8,
+        IBM = 9,
+        YTS = 10
+    }
+    
+    public class HttpHeaders {
+
+        public static HttpServerName ParseServerName(string name) {
+            return HttpServerName.Other;
+        }
+
+    }
+}
