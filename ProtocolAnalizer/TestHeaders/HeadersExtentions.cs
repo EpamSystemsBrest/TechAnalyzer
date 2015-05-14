352e11742ffdca0e30c82e58bb6fb4b9a426f61d
Mikhail_Barycheuski <Mikhail_Barycheuski@epam.com>
2015-05-14 15:23:02 +0300

    add TestHeader

---
+++ ProtocolAnalizer/TestHeaders/HeadersExtentions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolAnalizer.TestHeaders
+{
+    [Flags]
+    public enum HeadersTest
+    {
+        None = 0,
+        StrictTransportSecurity = 1 << 1,
+        XPoweredBy = 1 << 2,
+        Xhacker = 1 << 3,
+        XNananana = 1 << 4,
+        XPingback = 1 << 5,
+        XPickUsInstead = 1 << 6,
+        XPowered_By = 1 << 7,
+        XGeek = 1 << 8,
+        XAwesome = 1 << 9,
+        XServerNickName = 1 << 10,
+        XToynbeeIdea = 1 << 11,
+        XGasHost = 1 << 12,
+        XCookingWith = 1 << 13,
+        XGasolineAge = 1 << 14,
+        Guyito = 1 << 15,
+        XServedBy = 1 << 16,
+        XRecruiting = 1 << 17,
+        XCoreMission = 1 << 18,
+        XJobs = 1 << 19,
+        XHire = 1 << 20,
+        XSlogan = 1 << 21
+    }
+
+    public static class HeadersExtentions
+    {
+
+        public static Dictionary<string, HeadersTest> Headers = new Dictionary<string, HeadersTest>()
+        {
+            {@"not suppors this headers", HeadersTest.None},
+            {@"Strict-Transport-Security", HeadersTest.StrictTransportSecurity},
+            {@"X-PoweredBy", HeadersTest.XPoweredBy},
+            {@"X-hacker", HeadersTest.Xhacker},
+            {@"X-nananana", HeadersTest.XNananana},
+            {@"X-Pingback", HeadersTest.XPingback},
+            {@"X-PickUsInstead", HeadersTest.XPickUsInstead},
+            {@"X-Powered-By", HeadersTest.XPowered_By},
+            {@"X-Geek", HeadersTest.XGeek},
+            {@"X-Awesome", HeadersTest.XAwesome},
+            {@"X-ServerNickName", HeadersTest.XServerNickName},
+            {@"X-Toynbee-Idea", HeadersTest.XToynbeeIdea},
+            {@"X-GasHost", HeadersTest.XGasHost},
+            {@"X-Cooking-With", HeadersTest.XCookingWith},
+            {@"X-Gasoline-Age", HeadersTest.XGasolineAge},
+            {@"Guyito", HeadersTest.Guyito},
+            {@"X-Served-By", HeadersTest.XServedBy},
+            {@"X-Recruiting", HeadersTest.XRecruiting},
+            {@"X-Core-Mission", HeadersTest.XCoreMission},
+            {@"X-Jobs", HeadersTest.XJobs},
+            {@"X-Hire", HeadersTest.XHire},
+            {@"X-Slogan", HeadersTest.XSlogan},
+        };
+
+        public static Tuple<string, HeadersTest> TestHost(string host)
+        {
+            var httpResponse = GetResponse(host);
+            var result = Headers.Select(x => httpResponse.TestHostHeaders(x));
+
+            return Tuple.Create(host, result.Aggregate((x, y) => x | y));
+        }
+
+        private static HeadersTest TestHostHeaders(this HttpWebResponse response, KeyValuePair<string, HeadersTest> pair)
+        {
+            if (response.Headers[pair.Key] != null) return pair.Value;
+            return HeadersTest.None;
+        }
+
+        private static HttpWebResponse GetResponse(string host)
+        {
+            return (HttpWebResponse) WebRequest.Create(host).GetResponse();
+        }
+    }
+}
---
+++ ProtocolAnalizer/TestHeaders/HttpHeaders.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ProtocolAnalizer {
+
+    public enum HttpServerName { 
+        Other = 0,
+        Apache = 1,
+        IIS = 2,
+        ngnix = 3,
+        GSE = 4,
+        LiteSpeed = 5,
+        lighttpd = 6,
+        uServ = 7,
+        ATS = 8,
+        IBM = 9,
+        YTS = 10
+    }
+    
+    public class HttpHeaders {
+
+        static Regex regex = new Regex("(apache)|(iis)|(ngnix)|(gse)|(litespeed)|(lighttpd)|(userv)|(ats)|(ibm)|(yts)", RegexOptions.IgnoreCase);
+        public static HttpServerName ParseServerName(string name) {
+
+            if (name == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            string aaa = regex.Match(name).Value.ToLower();
+
+            switch (regex.Match(name).Value.ToLower())
+            {
+                case "apache": return HttpServerName.Apache;
+                case "iis": return HttpServerName.IIS;
+                case "ngnix": return HttpServerName.ngnix;
+                case "gse": return HttpServerName.GSE;
+                case "litespeed": return HttpServerName.LiteSpeed;
+                case "lighttpd": return HttpServerName.Apache;
+                case "userv": return HttpServerName.uServ;
+                case "ats": return HttpServerName.ATS;
+                case "ibm": return HttpServerName.IBM;
+                case "yts": return HttpServerName.YTS;
+                case "": return HttpServerName.Other;
+                default: throw new ArgumentException();
+            }
+        }
+
+    }
+}
