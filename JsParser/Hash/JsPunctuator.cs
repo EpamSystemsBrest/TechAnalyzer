cca3a437ef2ebdbd0450beef10497e66126be0f1
Misha Trotsiuk <mished@users.noreply.github.com>
2015-05-14 20:57:09 +0300

    SingleCharPunctuators added

---
+++ JsParser/Hash/JsPunctuator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace JsParser.Hash
+{
+    public class JsPunctuator
+    {
+        static readonly char[] SingleCharPunctuators = 
+        {
+            ' ', ',', '.', '(', ')', '[', ']', '{', '}', '/', '=', '+', '-', '*', '%', '&', '|', '^', '!', '~', '?', ':', '<', '>'
+        };
+
+        public static bool IsJsPunctuator(char c)
+        {
+            return SingleCharPunctuators.Contains(c);
+        }
+    }
+}
