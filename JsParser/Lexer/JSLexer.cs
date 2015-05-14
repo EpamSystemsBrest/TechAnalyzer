b7152fabbb65356bda2356a2a552d7c1134b85d0
MINSK\Ihar_Yachnik <Ihar_Yachnik@epam.com>
2015-05-14 17:16:33 +0300

    add JSLexer

---
+++ JsParser/Lexer/JSLexer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace JsParser.Lexer
+{
+    public class JSLexer
+    {
+        public readonly char[] content = new char[1024 * 1024];
+        private int length;
+        private int index;
+
+        public void Load(Stream stream, Encoding encoding)
+        {
+            using (var reader = new StreamReader(stream, encoding))
+            {
+                Load(reader);
+            }
+        }
+
+        public void Load(string code)
+        {
+            using (var reader = new StreamReader(code))
+            {
+                Load(reader);
+            }
+        }
+
+        public void Load(TextReader reader)
+        {
+            int startIndex = 0;
+            int bytesRead = 1;
+            while (bytesRead > 0)
+            {
+                bytesRead = reader.ReadBlock(content, startIndex, content.Length - startIndex);
+                startIndex += bytesRead;
+            }
+            length = startIndex;
+        }
+
+        public IEnumerable<JSToken> Parse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParsePunctuator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ParseKeyWord()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ JsParser/Lexer/JSToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JsParser.Hash;
+using ParserCommon;
+
+namespace JsParser.Lexer
+{
+    public struct JSToken
+    {
+        public JSKeyWord TokenType;
+        private char[] Source;
+        public StringSegment value;
+    }
+}
