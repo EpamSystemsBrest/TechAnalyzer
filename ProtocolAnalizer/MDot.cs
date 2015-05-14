6a5464693f84ff80bebd723e6125eec88cdb65e8
Mikhail-Borischewckij <mik.borischewskij@gmail.com>
2015-05-14 22:27:29 +0300

    add MDot

---
+++ ProtocolAnalizer/MDot.cs
@@ -0,0 +1,55 @@
+﻿using HtmlParser.Lexer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtocolAnalizer
+{
+    public class MDot
+    {
+        private const string DesktopUserAgent =
+            "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+
+        private const string MobileUserAgent =
+            "Mozilla/5.0 (Linux; U; Android 2.1; en-us; GT-I9000 Build/ECLAIR) AppleWebKit/525.10+ (KHTML, like Gecko) Version/3.0.4 Mobile Safari/523.12.2";
+
+        private static string GetResponseHost(IPHostEntry host, string userAgent)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.UserAgent.ParseAdd(userAgent);
+                var response = client.GetAsync("http://" + host.HostName).Result;
+                return response.RequestMessage.RequestUri.Host;
+            }
+        }
+
+        public static string MDotTest(IPHostEntry host)
+        {
+            var mobile = GetResponseHost(host, MobileUserAgent);
+            var desktop = GetResponseHost(host, DesktopUserAgent);
+
+            var lexer = new HtmlLexer();
+            lexer.Load(GetContent(host));
+
+            foreach (var item in lexer.Parse())
+            {
+                //TODO: сделать проверку на наличие <link rel="alternate" media="handheld, only screen and (max-width: 640px) и вытащить оттуда  href="url"
+                //Почему-то не до конца парсит файл
+
+            }
+            return desktop != mobile ? mobile : null;
+        }
+
+        private static string GetContent(IPHostEntry host)
+        {
+            using (var client = new WebClient())
+            {
+                return client.DownloadString("http://" + host.HostName);
+            }
+        }
+    }
+}
