738da65e6516f00c4d9dec4101970c480e0c3551
Mikchail-Borischewckij <mik.borischewskij@gmail.com>
2015-05-19 19:36:23 +0300

    ubdate

--- HtmlParser.Test/CompatibilityModeTest.cs
+++ HtmlParser.Test/CompatibilityModeTest.cs
@@ -10,9 +10,11 @@ namespace HtmlParser.Test
     public class CompatibilityModeTest
     {
         [Theory]
-        [InlineData(@"Doctype: ""-//w3c//dtd xhtml 1.0 transitional//en"" ""http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd""""""", CompatibilityModeDoctype.Quirks)]
+        [InlineData(@"-//w3c//dtd xhtml 1.0 transitional//en"" ""http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd""", CompatibilityModeDoctype.Quirks)]
         [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd html 4.01 transitional//en"" ""http://www.w3.org/tr/html4/loose.dtd""""", CompatibilityModeDoctype.LimitedQuirks)]
-        [InlineData(@"Doctype: ""doctype""", CompatibilityModeDoctype.Quirks)]
+        [InlineData(@"doctype", CompatibilityModeDoctype.Quirks)]
+        [InlineData(@"doctype html public", CompatibilityModeDoctype.Quirks)]
+        [InlineData(@"doctype html system", CompatibilityModeDoctype.Quirks)]
         [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd xhtml 1.0 transitional//en"" ""http://www.w3.org/tr/xhtml1/dtd/xhtml1-transitional.dtd""""", CompatibilityModeDoctype.LimitedQuirks)]
         [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd html 4.01 frameset//en"" ""http:www.w3.org/tr/htm14/frameset.dtd""""", CompatibilityModeDoctype.LimitedQuirks)]
         [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd xhtml 1.0 strict//en"" ""http://www.w3.org/tr/xhtml1/dtd/xhtml1-strict.dtd""""", CompatibilityModeDoctype.NoQuirks)]
--- HtmlParser/CompatibilityMode.cs
+++ HtmlParser/CompatibilityMode.cs
@@ -35,24 +35,25 @@ namespace HtmlParser
                 @"|(-//SoftQuad(\sSoftware)?//DTD HoTMetaL PRO (6.0|4.0)::(19990601|19971010)::extensions to HTML 4.0//)" +
                 @"|(-//Spyglass//DTD HTML 2.0 Extended//)|(-//SQ//DTD HTML 2.0 HoTMetaL \+ extensions//)" +
                 @"|(-//Sun Microsystems Corp.//DTD HotJava((\sStrict\s)|(\s))HTML//)" +
-                @"|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s3|\s[3-4].(2|2S|0))?(\s1995-03-24)?(\s(Draft|Final|Frameset|Transitional|Experimental|Strict))?(\s19960712|\s970421|\s3.0//EN|/EN)?//)" +
+                @"|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s3|\s[3-4].(2|2S|0))?(\s1995-03-24)?" +
+                @"(\s(Draft|Final|Frameset|Transitional|Experimental|Strict))?(\s19960712|\s970421|\s3.0//EN|/EN)?//)" +
                 @"|(-//WebTechs//DTD Mozilla HTML(\s2.0)?//)" +
                 @"|(-//""aol hometown//html 3.0 transitional//en)",
                 RegexOptions.IgnoreCase);
 
+        private static readonly Regex RegexForSpecificDoctype = new Regex(@"(doctype( html (public|system)?)?)$" +
+                                                                          @"|(""http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd"")");
+
         public static CompatibilityModeDoctype GetCompatibilityModeFromDoctype(string doctype)
         {
-            if (doctype.Equals(@"Doctype: ""doctype""") || doctype.Equals(@"Doctype: ""doctype html public""") ||
-                doctype.Equals(@"Doctype: ""doctype html system""") ||
-                doctype.Contains("http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd"))
-                return CompatibilityModeDoctype.Quirks;
+            if (RegexForSpecificDoctype.IsMatch(doctype)) return CompatibilityModeDoctype.Quirks;
 
             var separator = doctype.Contains("@@") ? '@' : '"';
             var item = doctype.Split(separator);
 
             if (RegexForQuirks.IsMatch(item[2]) || (Regex.IsMatch(item[2]) && item.Length == 5))
                 return CompatibilityModeDoctype.Quirks;
-            if (RegexForLimit.IsMatch(item[2]) || (Regex.IsMatch(item[2]) && item.Length == 7))
+            if (RegexForLimit.IsMatch(item[2]) || (Regex.IsMatch(item[2]) && item.Length != 5))
                 return CompatibilityModeDoctype.LimitedQuirks;
 
             return CompatibilityModeDoctype.NoQuirks;
