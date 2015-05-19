6fa636c14924dfeba7b7a0b8cbc39ab416b0d84b
Mikchail-Borischewckij <mik.borischewskij@gmail.com>
2015-05-19 17:43:18 +0300

    refactoring CompatibilityMode

--- HtmlParser.Test/CompatibilityModeTest.cs
+++ HtmlParser.Test/CompatibilityModeTest.cs
@@ -10,16 +10,16 @@ namespace HtmlParser.Test
     public class CompatibilityModeTest
     {
         [Theory]
-        [InlineData(@"Doctype: ""-//w3c//dtd xhtml 1.0 transitional//en"" ""http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd""""""", "Quirks")]
-        [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd html 4.01 transitional//en"" ""http://www.w3.org/tr/html4/loose.dtd""""", "Limited Quirks")]
-        [InlineData(@"Doctype: ""doctype""", "Quirks")]
-        [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd xhtml 1.0 transitional//en"" ""http://www.w3.org/tr/xhtml1/dtd/xhtml1-transitional.dtd""""", "Limited Quirks")]
-        [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd html 4.01 frameset//en"" ""http:www.w3.org/tr/htm14/frameset.dtd""""", "Limited Quirks")]
-        [InlineData( @"Doctype: ""doctype html public ""-//w3c//dtd xhtml 1.0 strict//en"" ""http://www.w3.org/tr/xhtml1/dtd/xhtml1-strict.dtd""""", "No Quirks")]
-        [InlineData( @"Doctype: ""doctype html public ""-//softquad//dtd hotmetal pro 4.0::19970916::extensions to html 4.0//en"" ""hmpro4.dtd""""", "No Quirks")]
-        [InlineData(@"Doctype: ""doctype html public ""-//ietf//dtd html//en""""", "Quirks")]
-      
-        public void Test_Compatibility_Mode_From_Doctype(string doctype, string expected)
+        [InlineData(@"Doctype: ""-//w3c//dtd xhtml 1.0 transitional//en"" ""http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd""""""", CompatibilityModeDoctype.Quirks)]
+        [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd html 4.01 transitional//en"" ""http://www.w3.org/tr/html4/loose.dtd""""", CompatibilityModeDoctype.LimitedQuirks)]
+        [InlineData(@"Doctype: ""doctype""", CompatibilityModeDoctype.Quirks)]
+        [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd xhtml 1.0 transitional//en"" ""http://www.w3.org/tr/xhtml1/dtd/xhtml1-transitional.dtd""""", CompatibilityModeDoctype.LimitedQuirks)]
+        [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd html 4.01 frameset//en"" ""http:www.w3.org/tr/htm14/frameset.dtd""""", CompatibilityModeDoctype.LimitedQuirks)]
+        [InlineData(@"Doctype: ""doctype html public ""-//w3c//dtd xhtml 1.0 strict//en"" ""http://www.w3.org/tr/xhtml1/dtd/xhtml1-strict.dtd""""", CompatibilityModeDoctype.NoQuirks)]
+        [InlineData(@"Doctype: ""doctype html public ""-//softquad//dtd hotmetal pro 4.0::19970916::extensions to html 4.0//en"" ""hmpro4.dtd""""", CompatibilityModeDoctype.NoQuirks)]
+        [InlineData(@"Doctype: ""doctype html public ""-//ietf//dtd html//en""""", CompatibilityModeDoctype.Quirks)]
+
+        public void Test_Compatibility_Mode_From_Doctype(string doctype, CompatibilityModeDoctype expected)
         {
             var actual =
                 CompatibilityMode.GetCompatibilityModeFromDoctype(doctype);
--- HtmlParser/CompatibilityMode.cs
+++ HtmlParser/CompatibilityMode.cs
@@ -8,30 +8,53 @@ using System.Threading.Tasks;
 
 namespace HtmlParser
 {
-    public class CompatibilityMode
+    public enum CompatibilityModeDoctype
     {
-        static int index = 0;
+        NoQuirks = 0,
+        Quirks = 1 << 1,
+        LimitedQuirks = 1 << 1,
 
-        public static string GetCompatibilityModeFromDoctype(string doctype)
+    }
+
+    public static class CompatibilityMode
+    {
+        private static readonly Regex Regex = new Regex("-//w3c//dtd html 4.01 (Transitional|Frameset)//(en)?",
+            RegexOptions.IgnoreCase);
+
+        private static readonly Regex RegexForLimit = new Regex(@"-//W3C//DTD XHTML 1.0 (Frameset|Transitional)//",
+            RegexOptions.IgnoreCase);
+
+        private static readonly Regex RegexForQuirks =
+            new Regex(
+                @"(-//IETF//DTD HTML(\s(2.0|2.1E|3.2|3.0|3|3.2 Final))?(\sStrict)?(\sLevel [0-3])?//)" +
+                @"|(\+//Silmaril//dtd html Pro v0r11 19970101//)|(-//AdvaSoft Ltd//DTD HTML 3.0 asWedit \+ extensions//)" +
+                @"|(-//AS//DTD HTML 3.0 asWedit \+ extensions//)|(-//Metrius//DTD Metrius Presentational//)" +
+                @"|(-//Microsoft//DTD Internet Explorer [2-3].0 (HTML Strict|HTML|Tables))" +
+                @"|(-//Netscape Comm. Corp.//DTD(\sStrict)? HTML)" +
+                @"|(-//O'Reilly and Associates//DTD HTML (2.0|((Extended|Extended Relaxed) 1.0)))" +
+                @"|(-//SoftQuad(\sSoftware)?//DTD HoTMetaL PRO (6.0|4.0)::(19990601|19971010)::extensions to HTML 4.0//)" +
+                @"|(-//Spyglass//DTD HTML 2.0 Extended//)|(-//SQ//DTD HTML 2.0 HoTMetaL \+ extensions//)" +
+                @"|(-//Sun Microsystems Corp.//DTD HotJava((\sStrict\s)|(\s))HTML//)" +
+                @"|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s3|\s[3-4].(2|2S|0))?(\s1995-03-24)?(\s(Draft|Final|Frameset|Transitional|Experimental|Strict))?(\s19960712|\s970421|\s3.0//EN|/EN)?//)" +
+                @"|(-//WebTechs//DTD Mozilla HTML(\s2.0)?//)" +
+                @"|(-//""aol hometown//html 3.0 transitional//en)",
+                RegexOptions.IgnoreCase);
+
+        public static CompatibilityModeDoctype GetCompatibilityModeFromDoctype(string doctype)
         {
             if (doctype.Equals(@"Doctype: ""doctype""") || doctype.Equals(@"Doctype: ""doctype html public""") ||
                 doctype.Equals(@"Doctype: ""doctype html system""") ||
-                doctype.Contains("http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd")) return "Quirks";
+                doctype.Contains("http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd"))
+                return CompatibilityModeDoctype.Quirks;
 
             var item = doctype.Split('"');
 
-            var regexForLimit = new Regex(@"-//W3C//DTD XHTML 1.0 (Frameset|Transitional)//", RegexOptions.IgnoreCase);
-            var regexForQuirks =
-                new Regex(
-                    @"(-//IETF//DTD HTML(\s(2.0|2.1E|3.2|3.0|3|3.2 Final))?(\sStrict)?(\sLevel [0-3])?//)|(\+//Silmaril//dtd html Pro v0r11 19970101//)|(-//AdvaSoft Ltd//DTD HTML 3.0 asWedit \+ extensions//)|(-//AS//DTD HTML 3.0 asWedit \+ extensions//)|(-//Metrius//DTD Metrius Presentational//)|(-//Microsoft//DTD Internet Explorer [2-3].0 (HTML Strict|HTML|Tables))|(-//Netscape Comm. Corp.//DTD(\sStrict)? HTML)|(-//O'Reilly and Associates//DTD HTML (2.0|((Extended|Extended Relaxed) 1.0)))|(-//SoftQuad(\sSoftware)?//DTD HoTMetaL PRO (6.0|4.0)::(19990601|19971010)::extensions to HTML 4.0//)|(-//Spyglass//DTD HTML 2.0 Extended//)|(-//SQ//DTD HTML 2.0 HoTMetaL \+ extensions//)|(-//Sun Microsystems Corp.//DTD HotJava((\sStrict\s)|(\s))HTML//)|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s3|\s[3-4].(2|2S|0))?(\s1995-03-24)?(\s(Draft|Final|Frameset|Transitional|Experimental|Strict))?(\s19960712|\s970421|\s3.0//EN|/EN)?//)|(-//WebTechs//DTD Mozilla HTML(\s2.0)?//)|(-//""aol hometown//html 3.0 transitional//en)",
-                    RegexOptions.IgnoreCase);
-            var r = new Regex("-//w3c//dtd html 4.01 (Transitional|Frameset)//(en)?", RegexOptions.IgnoreCase);
+            if (RegexForQuirks.IsMatch(item[2]) || (Regex.IsMatch(item[2]) && item.Length == 5))
+                return CompatibilityModeDoctype.Quirks;
+            if (RegexForLimit.IsMatch(item[2]) || (Regex.IsMatch(item[2]) && item.Length == 7))
+                return CompatibilityModeDoctype.LimitedQuirks;
 
-            if (regexForQuirks.IsMatch(item[2]) || (r.IsMatch(item[2]) && item.Length == 5)) return "Quirks";
-            if (regexForLimit.IsMatch(item[2]) || (r.IsMatch(item[2]) && item.Length == 7)) return "Limited Quirks";
-
-            return "No Quirks";
+            return CompatibilityModeDoctype.NoQuirks;
         }
-
     }
 }
