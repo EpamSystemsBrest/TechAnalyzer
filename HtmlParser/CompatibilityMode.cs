25c53230c05c9c658b9a342d1ac134f2ea159dda
Mikhail_Barycheuski <Mikhail_Barycheuski@epam.com>
2015-05-20 19:21:42 +0300

    fixt

--- HtmlParser/CompatibilityMode.cs
+++ HtmlParser/CompatibilityMode.cs
@@ -19,16 +19,16 @@ namespace HtmlParser
 
     public static class CompatibilityMode
     {
-        private static readonly char[] Simbol = new[] {'@', '\'','"'};
-        private static readonly Regex Regex = new Regex("-//w3c//dtd html 4.01 (Transitional|Frameset)//(en)?$",
+        private static readonly char[] Simbol = {'@', '\'','"'};
+        private static readonly Regex Regex = new Regex("-//w3c//dtd html 4.01 (Transitional|Frameset)//(en)?",
             RegexOptions.IgnoreCase);
 
-        private static readonly Regex RegexForLimit = new Regex(@"-//W3C//DTD XHTML 1.0 (Frameset|Transitional)//",
+        private static readonly Regex RegexForLimit = new Regex(@"(-//W3C//DTD XHTML 1.0 (Frameset|Transitional)//(en)?)|(-//w3c//dtd html 4.01 (Transitional|Frameset)//$)",
             RegexOptions.IgnoreCase);
 
         private static readonly Regex RegexForQuirks =
             new Regex(
-                @"(-//IETF//DTD HTML(\s(2.0|2.1E|3.2|3.0|3|3.2 Final))?(\sStrict)?(\sLevel [0-3])?//)" +
+                @"(-//IETF//DTD HTML(\s(2.0|2.1E|3.2|3.0|3|3.2 Final))?(\sStrict)?(\sLevel [0-3])?//(en)?&)" +
                 @"|(\+//Silmaril//dtd html Pro v0r11 19970101//)|(-//AdvaSoft Ltd//DTD HTML 3.0 asWedit \+ extensions//)" +
                 @"|(-//AS//DTD HTML 3.0 asWedit \+ extensions//)|(-//Metrius//DTD Metrius Presentational//)" +
                 @"|(-//Microsoft//DTD Internet Explorer [2-3].0 (HTML Strict|HTML|Tables))" +
@@ -37,30 +37,40 @@ namespace HtmlParser
                 @"|(-//SoftQuad(\sSoftware)?//DTD HoTMetaL PRO (6.0|4.0)::(19990601|19971010)::extensions to HTML 4.0//(en)?)" +
                 @"|(-//Spyglass//DTD HTML 2.0 Extended//)|(-//SQ//DTD HTML 2.0 HoTMetaL \+ extensions//)" +
                 @"|(-//Sun Microsystems Corp.//DTD HotJava((\sStrict\s)|(\s))HTML//)" +
-                @"|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s3|\s[3-4].(2|2S|0))?(\s1995-03-24)?" +
-                @"(\s(Draft|Final|Frameset|Transitional|Experimental|Strict))?(\s19960712|\s970421|\s3.0//EN|/EN)?//)" +
+                //@"|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s3|\s[3-4].(2|2S|0))?(\s1995-03-24)?" +
+                //@"(\s(Draft|Final|Frameset|Transitional|Experimental|Strict))?(\s19960712|\s970421|\s3.0//EN|/EN)?//)" +
+                @"(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML\s((3 1995-03-24)|(3.2 Draft)|(3.2 Final)|(3.2)|(3.2S Draft)|(4.0 Frameset)|(4.0 Transitional)|(Experimental 19960712)|(Experimental 970421)|(3.0)|(Strict 3.0))//)"+
                 @"|(-//WebTechs//DTD Mozilla HTML(\s2.0)?//)" +
                 @"|(-//""aol hometown//html 3.0 transitional//en)",
                 RegexOptions.IgnoreCase);
 
         private static readonly Regex RegexForSpecificDoctype = new Regex(@"(doctype( html (public|system)?)?)$" +
-                                                                          @"|(""http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd"")",RegexOptions.IgnoreCase);
+                                                                          @"|(""http://www.ibm.com/data/dtd/v11/ibmxhtml1-transitional.dtd"")", RegexOptions.IgnoreCase);
 
         public static CompatibilityModeDoctype GetCompatibilityModeFromDoctype(string doctype)
         {
+
+            if (doctype.Equals("doctype html") || doctype.Equals(@"doctype html public """"")) return CompatibilityModeDoctype.NoQuirks;
+
             if (RegexForSpecificDoctype.IsMatch(doctype)) return CompatibilityModeDoctype.Quirks;
 
             var separator = Simbol.Where(doctype.Contains).FirstOrDefault();
-            var item = doctype.Split(separator);
+            var items =
+                doctype.Split(separator).Where(x => !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x)).ToArray();
 
-            if (RegexForQuirks.IsMatch(item[1]) || (Regex.IsMatch(item[1]) && item.Length <= 3) ||
-                (separator == Simbol[0] && (RegexForQuirks.IsMatch(item[2]))))
+            var publicId = items.ElementAt(1);
+         
+            if (RegexForQuirks.IsMatch(publicId) || (Regex.IsMatch(publicId) && items.Count() < 3))
                 return CompatibilityModeDoctype.Quirks;
+            return RegexForLimit.IsMatch(publicId)
+                ? CompatibilityModeDoctype.LimitedQuirks
+                : CompatibilityModeDoctype.NoQuirks;
 
-            if (RegexForLimit.IsMatch(item[1]) || (Regex.IsMatch(item[1]) && item.Length > 3))
-                return CompatibilityModeDoctype.LimitedQuirks;
 
-            return CompatibilityModeDoctype.NoQuirks;
+            //if (RegexForQuirks.IsMatch(doctype) && Regex.IsMatch(doctype))
+            //    return CompatibilityModeDoctype.Quirks;
+            //if (RegexForLimit.IsMatch(doctype))
+            //    return CompatibilityModeDoctype.LimitedQuirks;
         }
     }
 }
