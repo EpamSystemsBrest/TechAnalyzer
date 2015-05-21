7d05f309f536a3acb4905e5d0cfee9308c595dc8
Mikhail_Barycheuski <mik.borischewskij@gmail.com>
2015-05-21 19:05:41 +0300

    ubdate

--- HtmlParser/CompatibilityMode.cs
+++ HtmlParser/CompatibilityMode.cs
@@ -28,7 +28,8 @@ namespace HtmlParser
 
         private static readonly Regex RegexForQuirks =
             new Regex(
-                @"(-//IETF//DTD HTML(\s(2.0|2.1E|3.2|3.0|3|3.2 Final))?(\sStrict)?(\sLevel [0-3])?//(en)?)" +
+                //@"(-//IETF//DTD HTML(\s(2.0|2.1E|3.2|3.0|3|3.2 Final))?(\sStrict)?(\sLevel [0-3])?//(en)?)" +
+                @"(-//IETF//DTD HTML(\s(2.0 Level [1-2]|2.0 Strict Level [1-2]|2.0 Strict|2.0|2.1E|3.0|3.2 Final|3.2|3|Level [0-3]|Strict Level [0-3]|Strict))?//(en)?)"+
                 @"|(\+//Silmaril//dtd html Pro v0r11 19970101//)|(-//AdvaSoft Ltd//DTD HTML 3.0 asWedit \+ extensions//)" +
                 @"|(-//AS//DTD HTML 3.0 asWedit \+ extensions//)|(-//Metrius//DTD Metrius Presentational//)" +
                 @"|(-//Microsoft//DTD Internet Explorer [2-3].0 (HTML Strict|HTML|Tables))" +
@@ -39,7 +40,7 @@ namespace HtmlParser
                 @"|(-//Sun Microsystems Corp.//DTD HotJava((\sStrict\s)|(\s))HTML//)" +
                 //@"|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s3|\s[3-4].(2|2S|0))?(\s1995-03-24)?" +
                 //@"(\s(Draft|Final|Frameset|Transitional|Experimental|Strict))?(\s19960712|\s970421|\s3.0//EN|/EN)?//)" +
-                @"|(-(//|/)(W3C|W3O)(//|/)DTD(\sW3)? HTML(\s((3 1995-03-24)|(3.2 Draft)|(3.2 Final)|(3.2)|(3.2S Draft)|(4.0 Frameset)|(4.0 Transitional)|(Experimental 19960712)|(Experimental 970421)|(3.0)|(Strict 3.0)))?(//|/)(en)?)" +
+                @"(-(//|/)W3C(//|/)DTD HTML(\s((3 1995-03-24)|(3.2 Draft)|(3.2 Final)|(3.2)|(3.2S Draft)|(4.0 Frameset)|(4.0 transitional)|(Experimental 19960712)|(Experimental 970421)|(Strict 3.0)))(//|/)(en)?)|(-(//|/)W3O(//|/)DTD W3 HTML (3.0)?//(en)?)" +
                 @"|(-//WebTechs//DTD Mozilla HTML(\s2.0)?//)", // +
                 //@"|(aol hometown//html 3.0 transitional//en)",
                 RegexOptions.IgnoreCase);
@@ -50,7 +51,7 @@ namespace HtmlParser
         public static CompatibilityModeDoctype GetCompatibilityModeFromDoctype(string doctype)
         {
 
-            if (doctype.Equals("doctype html") || doctype.Equals(@"doctype html public """""))
+            if (doctype.Equals("doctype html") || doctype.Equals(@"doctype html public """))
                 return CompatibilityModeDoctype.NoQuirks;
 
             if (RegexForSpecificDoctype.IsMatch(doctype)) return CompatibilityModeDoctype.Quirks;
@@ -60,11 +61,10 @@ namespace HtmlParser
                 doctype.Split(separator)
                     .Where(
                         x =>
-                            !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x) && x != "-//" && x != " <html" &&
-                            x != " /");
+                            !string.IsNullOrEmpty(x) && !string.IsNullOrWhiteSpace(x)).ToArray();
 
             var publicId = items.ElementAt(1);
-
+     
             if (RegexForQuirks.IsMatch(publicId) || (Regex.IsMatch(publicId) && items.Count() < 3))
                 return CompatibilityModeDoctype.Quirks;
             return RegexForLimit.IsMatch(publicId)
